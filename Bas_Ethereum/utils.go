fef61a427656aed94ce0b88389b7f795041c0cef
= <=>
2020-04-13 20:15:09 +0800

    change access point

--- Bas_Ethereum/utils.go
+++ Bas_Ethereum/utils.go
@@ -24,9 +24,9 @@ func GetHash(key string) Hash{
 }
 
 var AccessPoints = []string{
-"wss://ropsten.infura.io/ws/v3/831ab04fa4964991b5fba5c52106d7b0",
-"wss://ropsten.infura.io/ws/v3/8b8db3cca50a4fcf97173b7619b1c4c3",
-"ws://75.135.96.248:3334",
+"wss://mainnet.infura.io/ws/v3/831ab04fa4964991b5fba5c52106d7b0",
+"wss://mainnet.infura.io/ws/v3/8b8db3cca50a4fcf97173b7619b1c4c3",
+//"ws://75.135.96.248:3334",
 }
 
 
